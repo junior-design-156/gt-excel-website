f8b279fc875eed4c91577afd0c0a84540539c8f0
Sarah Bush <skbush13@gmail.com>
2017-01-23 11:19:01 -0500

    moved index to view

--- routes/router.js
+++ routes/router.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.use(express.static(__dirname + '/view'));
+router.use(express.static(__dirname + '/../view'));
 
 router.route('/') 
     .get(function(req, res) {
--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 'use strict';
 var express = require('express');
-var router = require('./router');
+var bodyParser = require('body-parser');
+var router = require('./routes/router');
 
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', router);
 
 app.use(function (err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).send('Sorry, there was an error processing your request')
+  if(err) {
+    console.error(err.stack);
+    res.status(500).send('Sorry, there was an error processing your request');
+  }
 });
 
 app.listen(8080, function() {
